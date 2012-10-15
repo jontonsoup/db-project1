abec66e16a58e5f44f4eb150b864285cc301bf14
Jonathan Friedman <friedmanj98@gmail.com>
2012-10-15 17:01:35 -0500

    Feature: dynamic cycles

--- rwb.pl
+++ rwb.pl
@@ -233,8 +233,8 @@ if (defined(param("act"))) {
                   my $cookie=cookie(-name=>$cookiename,
                     -value=>$outputcookiecontent,
                     -expires=>($deletecookie ? '-1h' : '+1h'));
-push @outputcookies, $cookie;
-}
+                  push @outputcookies, $cookie;
+                }
 #
 # We also send back a debug cookie
 #
@@ -242,7 +242,7 @@ push @outputcookies, $cookie;
 if (defined($outputdebugcookiecontent)) {
   my $cookie=cookie(-name=>$debugcookiename,
     -value=>$outputdebugcookiecontent);
-push @outputcookies, $cookie;
+  push @outputcookies, $cookie;
 }
 
 #
@@ -345,6 +345,12 @@ if ($action eq "base") {
   #
   print "<div id=\"map\" style=\"width:100\%; height:80\%\"></div>";
 
+
+  my @rows;
+  eval { @rows = ExecSQL($dbuser, $dbpasswd, "select unique cycle from cs339.committee_master", "COL"); };
+  use Data::Dumper;
+  print Dumper(@rows);
+
   print start_form(-name=>'selections'),
   checkbox(-name=>'Committees', -id=>'committees'),p,
   checkbox(-name=>'Individuals', -id=>'individuals'),p,
@@ -352,9 +358,9 @@ if ($action eq "base") {
   checkbox(-name=>'Opinions', -id=>'opinions'),p,
   popup_menu(-name=>'',
     -multiple=>'true',
-    -values=>['1010'],
+    -values=>[@rows],
     -default=>'1010'),p,
-end_form;
+  end_form;
 
 #
 # And a div to populate with info about nearby stuff
@@ -437,65 +443,65 @@ if ($debug) {
        candidates => 1,
        individuals =>1,
        opinions => 1);
-} else {
-  map {$what{$_}=1} split(/\s*,\s*/,$whatparam);
-}
+      } else {
+        map {$what{$_}=1} split(/\s*,\s*/,$whatparam);
+      }
 
 
-if ($what{committees}) {
-  my ($str,$error) = Committees($latne,$longne,$latsw,$longsw,$cycle,$format);
-  if (!$error) {
-    if ($format eq "table") {
-     print "<h2>Nearby committees</h2>$str";
-     } else {
-       print $str;
+      if ($what{committees}) {
+        my ($str,$error) = Committees($latne,$longne,$latsw,$longsw,$cycle,$format);
+        if (!$error) {
+          if ($format eq "table") {
+           print "<h2>Nearby committees</h2>$str";
+           } else {
+             print $str;
+           }
+         }
+       }
+       if ($what{candidates}) {
+        my ($str,$error) = Candidates($latne,$longne,$latsw,$longsw,$cycle,$format);
+        if (!$error) {
+          if ($format eq "table") {
+           print "<h2>Nearby candidates</h2>$str";
+           } else {
+             print $str;
+           }
+         }
+       }
+       if ($what{individuals}) {
+        my ($str,$error) = Individuals($latne,$longne,$latsw,$longsw,$cycle,$format);
+        if (!$error) {
+          if ($format eq "table") {
+           print "<h2>Nearby individuals</h2>$str";
+           } else {
+             print $str;
+           }
+         }
+       }
+       if ($what{opinions}) {
+        my ($str,$error) = Opinions($latne,$longne,$latsw,$longsw,$cycle,$format);
+        if (!$error) {
+          if ($format eq "table") {
+           print "<h2>Nearby opinions</h2>$str";
+           } else {
+             print $str;
+           }
+         }
+       }
      }
-   }
- }
- if ($what{candidates}) {
-  my ($str,$error) = Candidates($latne,$longne,$latsw,$longsw,$cycle,$format);
-  if (!$error) {
-    if ($format eq "table") {
-     print "<h2>Nearby candidates</h2>$str";
-     } else {
-       print $str;
-     }
-   }
- }
- if ($what{individuals}) {
-  my ($str,$error) = Individuals($latne,$longne,$latsw,$longsw,$cycle,$format);
-  if (!$error) {
-    if ($format eq "table") {
-     print "<h2>Nearby individuals</h2>$str";
-     } else {
-       print $str;
-     }
-   }
- }
- if ($what{opinions}) {
-  my ($str,$error) = Opinions($latne,$longne,$latsw,$longsw,$cycle,$format);
-  if (!$error) {
-    if ($format eq "table") {
-     print "<h2>Nearby opinions</h2>$str";
-     } else {
-       print $str;
-     }
-   }
- }
-}
 
 
-if ($action eq "invite-user") {
-  print h2("Invite User Functionality Is Unimplemented");
-}
+     if ($action eq "invite-user") {
+      print h2("Invite User Functionality Is Unimplemented");
+    }
 
-if ($action eq "give-opinion-data") {
-  print h2("Giving Location Opinion Data Is Unimplemented");
-}
+    if ($action eq "give-opinion-data") {
+      print h2("Giving Location Opinion Data Is Unimplemented");
+    }
 
-if ($action eq "give-cs-ind-data") {
-  print h2("Giving Crowd-sourced Individual Geolocations Is Unimplemented");
-}
+    if ($action eq "give-cs-ind-data") {
+      print h2("Giving Crowd-sourced Individual Geolocations Is Unimplemented");
+    }
 
 #
 # ADD-USER
@@ -735,11 +741,11 @@ if ($action eq "add-user") {
                     return (MakeTable("committee_data","2D",
                      ["latitude", "longitude", "name", "party", "street1", "street2", "city", "state", "zip"],
                      @rows),$@);
-} else {
-  return (MakeRaw("committee_data","2D",@rows),$@);
-}
-}
-}
+                    } else {
+                      return (MakeRaw("committee_data","2D",@rows),$@);
+                    }
+                  }
+                }
 
 
 #
@@ -761,11 +767,11 @@ sub Candidates {
         return (MakeTable("candidate_data", "2D",
          ["latitude", "longitude", "name", "party", "street1", "street2", "city", "state", "zip"],
          @rows),$@);
-} else {
-  return (MakeRaw("candidate_data","2D",@rows),$@);
-}
-}
-}
+        } else {
+          return (MakeRaw("candidate_data","2D",@rows),$@);
+        }
+      }
+    }
 
 
 #
@@ -790,11 +796,11 @@ sub Individuals {
         return (MakeTable("individual_data", "2D",
          ["latitude", "longitude", "name", "city", "state", "zip", "employer", "amount"],
          @rows),$@);
-} else {
-  return (MakeRaw("individual_data","2D",@rows),$@);
-}
-}
-}
+        } else {
+          return (MakeRaw("individual_data","2D",@rows),$@);
+        }
+      }
+    }
 
 
 #
@@ -817,11 +823,11 @@ sub Opinions {
         return (MakeTable("opinion_data","2D",
          ["latitude", "longitude", "name", "city", "state", "zip", "employer", "amount"],
          @rows),$@);
-} else {
-  return (MakeRaw("opinion_data","2D",@rows),$@);
-}
-}
-}
+        } else {
+          return (MakeRaw("opinion_data","2D",@rows),$@);
+        }
+      }
+    }
 
 
 #
@@ -839,8 +845,8 @@ sub PermTable {
         "2D",
         ["Perm"],
         @rows),$@);
-}
-}
+    }
+  }
 
 #
 # Generate a table of users
@@ -857,8 +863,8 @@ sub UserTable {
         "2D",
         ["Name", "Email"],
         @rows),$@);
-}
-}
+    }
+  }
 
 #
 # Generate a table of users and their permissions
@@ -875,8 +881,8 @@ sub UserPermTable {
         "2D",
         ["Name", "Permission"],
         @rows),$@);
-}
-}
+    }
+  }
 
 #
 # Add a user
@@ -889,7 +895,7 @@ sub UserPermTable {
 sub UserAdd {
   eval { ExecSQL($dbuser,$dbpasswd,
    "insert into rwb_users (name,password,email,referer) values (?,?,?,?)",undef,@_);};
-return $@;
+  return $@;
 }
 
 #
@@ -912,7 +918,7 @@ sub UserDel {
 sub GiveUserPerm {
   eval { ExecSQL($dbuser,$dbpasswd,
    "insert into rwb_permissions (name,action) values (?,?)",undef,@_);};
-return $@;
+  return $@;
 }
 
 #
@@ -925,7 +931,7 @@ return $@;
 sub RevokeUserPerm {
   eval { ExecSQL($dbuser,$dbpasswd,
    "delete from rwb_permissions where name=? and action=?",undef,@_);};
-return $@;
+  return $@;
 }
 
 #
